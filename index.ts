a51d4b138c66d680c9277b973a2fed3ac323c118
Cancellier27 <filipecancelliercosta@gmail.com>
2025-05-09 20:44:40 +0100

    added the dates on cards, and sorted from newest to oldest

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-// Define DOM elements
 const form = document.getElementById("search-form") as HTMLFormElement
 const usernameInput = document.getElementById(
   "username-input"
@@ -7,7 +6,7 @@ const usernameInput = document.getElementById(
 const results = document.getElementById("results") as HTMLBodyElement
 const userNotFound = document.getElementById("user-not-found") as HTMLElement
 const avatar = document.getElementById("avatar") as HTMLImageElement
-const nameEl = document.getElementById("name") as HTMLElement
+const nameLink = document.getElementById("name-link") as HTMLLinkElement
 const bioEl = document.getElementById("bio") as HTMLElement
 const locationEl = document.getElementById("location") as HTMLElement
 const companyEl = document.getElementById("company") as HTMLElement
@@ -24,38 +23,45 @@ interface GitHubUser {
   company: string
   followers: number
   following: number
+  html_url: string
 }
 
 interface GitHubRepo {
   name: string
   html_url: string
+  updated_at: string
 }
 
 form.addEventListener("submit", async (event: Event) => {
   try {
     event.preventDefault()
+    // hide the previous search UI
     results.style.display = "none"
+    // get username input and return is none was inserted
     const username = usernameInput.value.trim()
     if (!username) return
 
     clearData()
 
-    const [userRes, reposRes, starredRes] = await Promise.all([
+    // fetch all information separately
+    const [userResponse, reposResponse, starredResponse] = await Promise.all([
       fetch(`https://api.github.com/users/${username}`),
       fetch(`https://api.github.com/users/${username}/repos`),
       fetch(`https://api.github.com/users/${username}/starred`)
     ])
 
-    if (!userRes.ok) {
+    if (!userResponse.ok) {
       // show error message
       userNotFound.style.display = "flex"
       throw new Error("User not found")
     }
 
-    const userData: GitHubUser = await userRes.json()
-    const reposData: GitHubRepo[] = await reposRes.json()
-    const starredData: GitHubRepo[] = await starredRes.json()
+    // get json data from the responses
+    const userData: GitHubUser = await userResponse.json()
+    const reposData: GitHubRepo[] = await reposResponse.json()
+    const starredData: GitHubRepo[] = await starredResponse.json()
 
+    // run functions to populate the DOM
     populateUserProfile(userData)
     populateLists(reposData, repoList, "repo")
     populateLists(starredData, starredList, "star")
@@ -68,9 +74,10 @@ form.addEventListener("submit", async (event: Event) => {
   }
 })
 
+// clear DOM elements
 function clearData() {
   avatar.src = ""
-  nameEl.textContent = ""
+  nameLink.textContent = ""
   bioEl.textContent = ""
   locationEl.textContent = ""
   companyEl.textContent = ""
@@ -80,14 +87,16 @@ function clearData() {
   starredList.innerHTML = ""
 }
 
+// Populate the user tags
 function populateUserProfile(user: GitHubUser) {
   avatar.src = user.avatar_url
-  nameEl.textContent = user.name || "No name provided"
+  nameLink.textContent = user.name || "No name provided"
   bioEl.textContent = user.bio || ""
   locationEl.textContent = user.location || ""
   companyEl.textContent = user.company || ""
   followersEl.textContent = `Followers: ${user.followers}`
   followingEl.textContent = `Following: ${user.following}`
+  nameLink.href = user.html_url
 }
 
 function populateLists(
@@ -97,7 +106,15 @@ function populateLists(
 ) {
   container.innerHTML = "" // Clear old content
 
-  if (repos.length === 0) {
+  // sort the repos array from newest to oldest
+  const sortedRepos = repos.sort(function(a: GitHubRepo, b: GitHubRepo): number {
+    let date_A: number = new Date(a.updated_at).getTime()
+    let date_B: number = new Date(b.updated_at).getTime()
+    return date_B - date_A
+  })
+
+  if (sortedRepos.length === 0) {
+    // if there is no repos or no starred repos, create a card saying that no repos were found
     let message = type === "star" ? "starred " : ""
 
     const card = document.createElement("div")
@@ -111,17 +128,31 @@ function populateLists(
     container.appendChild(card)
     return
   } else {
-    repos.forEach((repo) => {
+    sortedRepos.forEach((repo) => {
+      // create card tag
       const card = document.createElement("div")
       card.className = "repo-card"
 
+      // create link tag
       const link = document.createElement("a")
       link.href = repo.html_url
       link.textContent = repo.name
       link.target = "_blank"
 
+      // create paragraph tag
+      const date = document.createElement("p")
+      // format the date to be like YYYY/MM/DD
+      let lastUpdate = repo.updated_at.split("T")[0].split("-").join("/")
+      date.textContent = lastUpdate
+
+      // append both elements to card
       card.appendChild(link)
+      card.appendChild(date)
+
+      // append card to the main container
       container.appendChild(card)
     })
   }
 }
+
+
